d313ff4c25d40f3a0856b2fbb48aa199e9708eab
Nikodem Hirsch <niko1326.hirsch@gmail.com>
2024-10-07 09:28:13 +0200

    First commit

---
+++ MPR_Project/src/main/java/com/example/MPR_Project/MprProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.MPR_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprProjectApplication.class, args);
+	}
+
+}
